231cf1aaf1160676761c86c2ea3e6f84ab112372
nl-uni-proj <168341622+nl-uni-proj@users.noreply.github.com>
2024-05-24 14:11:46 +0300

    theme x text matrix

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,62 @@
 mod ansi;
 mod lexer;
 
+const TEXT_COUNT: usize = 10;
+const THEME_COUNT: usize = 3;
+
+const THEME_NAMES: [&'static str; THEME_COUNT] = ["rust_docs", "lex_wiki", "hobbit_book"];
+const THEME_TEXT_COUNTS: [usize; THEME_COUNT] = [3, 4, 3];
+
+const THEME_TEXT_MATRIX: [[usize; TEXT_COUNT]; THEME_COUNT] = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+];
+
 fn main() {
-    let rust_docs = ThemeGroup::new("rust_docs", 1, 3);
-    let lex_wiki = ThemeGroup::new("lex_wiki", 2, 4);
-    let hobbit_book = ThemeGroup::new("hobbit_book", 3, 3);
-
-    analyze_group(rust_docs);
-    analyze_group(lex_wiki);
-    analyze_group(hobbit_book);
-}
-
-struct ThemeGroup {
-    name: &'static str,
-    group_id: usize,
-    text_count: usize,
-}
-
-impl ThemeGroup {
-    fn new(name: &'static str, group_id: usize, text_count: usize) -> ThemeGroup {
-        ThemeGroup {
-            name,
-            group_id,
-            text_count,
-        }
+    for theme_idx in 0..THEME_COUNT {
+        analyze_group_theme(theme_idx);
     }
+    pretty_print_theme_text_matrix();
+    println!("");
 }
 
-fn analyze_group(group: ThemeGroup) {
-    let cwd = std::env::current_dir().expect("failed to get cwd");
+fn analyze_group_theme(theme_idx: usize) {
+    let cwd: std::path::PathBuf = std::env::current_dir().expect("failed to get cwd");
 
-    for file_idx in 1..=group.text_count {
-        let filename = format!("text/{}_{file_idx}.txt", group.group_id);
+    for text_idx in 0..THEME_TEXT_COUNTS[theme_idx] {
+        let filename = format!("text/{}_{}.txt", theme_idx + 1, text_idx + 1);
         let filepath = cwd.join(filename);
         let words = lexer::lex_stemmed(filepath);
 
-        pretty_print_words(&group, file_idx, &words);
+        pretty_print_words(theme_idx, text_idx, &words);
     }
 }
 
-fn pretty_print_words(group: &ThemeGroup, file_idx: usize, words: &[String]) {
+fn pretty_print_theme_text_matrix() {
     let g = ansi::GREEN_BOLD;
     let r = ansi::RESET;
-    println!("\n{g}GROUP: `{}`, FILE: `{file_idx}`{r}", group.name);
+    println!("\n{g}THEME x TEXT MATRIX:{r}");
+
+    print!("{:12}", "text index");
+    for text_idx in 0..TEXT_COUNT {
+        print!("{:2} ", text_idx);
+    }
+    print!("\n");
+
+    for theme_idx in 0..THEME_COUNT {
+        let theme_name = THEME_NAMES[theme_idx];
+        println!("{:12}{:?}", theme_name, THEME_TEXT_MATRIX[theme_idx]);
+    }
+}
+
+fn pretty_print_words(theme_idx: usize, text_idx: usize, words: &[String]) {
+    let g = ansi::GREEN_BOLD;
+    let r = ansi::RESET;
+    println!(
+        "\n{g}GROUP: `{}`, TEXT: `{text_idx}`{r}",
+        THEME_NAMES[theme_idx]
+    );
 
     const PRINT_WIDTH: usize = 60;
     let mut width = 0;
